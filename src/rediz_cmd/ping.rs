5f3735c008e4f9a62a8e8cba0991814b09103d9d
cezirer <retinasliver@163.com>
2026-05-25 22:42:19 +0800

    codecrafters submit [skip ci]

--- src/rediz_cmd/ping.rs
+++ src/rediz_cmd/ping.rs
@@ -6,8 +6,8 @@ pub struct PingCmd;
 
 impl Cmd for PingCmd {
     fn execute(&self, argv: Vec<Value>, stream: &mut TcpStream) -> Result<(), std::io::Error> {
-        let bulk_s = encode(&Value::Bulk("PONG".to_string()));
-        stream.write_all(&bulk_s.as_slice())?;
+        // let bulk_s = encode(&Value::Bulk("PONG".to_string()));
+        stream.write_all(b"PONG")?;
         Ok(())
     }
 }
